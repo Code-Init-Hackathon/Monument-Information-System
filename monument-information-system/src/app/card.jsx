7da64bce5fdfbe653b78523121fe919f7c864a85
sreeharij <sreeharij2003@gmail.com>
2024-06-25 01:16:59 +0530

    change

--- monument-information-system/src/app/card.jsx
+++
@@ -1,8 +0,0 @@
-export default function Card({imageUrl,name}){
-    return(
-        <div className="card">
-            <img src={imageUrl} alt={name} className="card-image" />
-            <div className="card-name">{name}</div>
-        </div>
-    );
-}
